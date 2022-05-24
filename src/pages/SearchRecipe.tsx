818f5e7e05607cb0d088b972f485340807a37c34
Colin Edwards <ColinJhEdwards@gmail.com>
2022-05-23 18:37:53 -0700

    installed html react parser to avoid using dangerouslySetInnerHTML

--- src/pages/SearchRecipe.tsx
+++ src/pages/SearchRecipe.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, FormEvent } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import NoResultMessage from "../components/NoResultMessage";
